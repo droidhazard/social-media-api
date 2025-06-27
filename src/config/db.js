0eb70b7ee22977da72d284847e07db378b0a9db9
Zohaib Ali <www.zohaib.0335@gmail.com>
2025-06-27 22:53:06 +0500

    environment-aware connection

--- src/config/db.js
+++ src/config/db.js
@@ -3,6 +3,11 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const environment =
+  process.env.ENVIRONMENT === "development"
+    ? process.env.MONGO_LOCAL_URI
+    : process.env.MONGO_ATLAS_URI;
+
 const connectDB = async () => {
   mongoose.connection.on("error", (error) => {
     console.log("^^^mongodb connection FAILED");
@@ -10,7 +15,7 @@ const connectDB = async () => {
   mongoose.connection.on("connected", (message) => {
     console.log("^^^mongodb connection SUCCESSFULL");
   });
-  const connection = await mongoose.connect(process.env.MONGO_LOCAL_URI);
+  const connection = await mongoose.connect(environment);
 };
 
 export { connectDB };
